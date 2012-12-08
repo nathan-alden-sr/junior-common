f98319a0ae093acf289ac7ff18328e3478d01859
Nathan Alden <visualcsharpcoder@hotmail.com>
2012-12-07 18:12:12 -0600

    Collapsed path::combine calls
    Updated NAnt build files to 2.6.2
    Removed CommonAssemblyInfo.cs

--- Source/Common/Properties/AssemblyInfo.cs
+++ Source/Common/Properties/AssemblyInfo.cs
@@ -1,3 +1,7 @@
 ﻿using System.Reflection;
+using System.Runtime.InteropServices;
 
+[assembly:ComVisible(false)]
 [assembly:AssemblyTitle("Junior.Common")]
+[assembly:AssemblyVersion("2.0.0.0")]
+[assembly:AssemblyFileVersion("2.0.1.0")]
