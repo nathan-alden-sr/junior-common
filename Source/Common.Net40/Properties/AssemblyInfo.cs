bbc837701f526b2e7253694a8409f2486c4f67b8
Nathan Alden, Sr <me@nathanalden.com>
2014-06-13 08:38:15 -0500

    Fixed nuspec targets

--- Source/Common.Net40/Properties/AssemblyInfo.cs
+++ Source/Common.Net40/Properties/AssemblyInfo.cs
@@ -2,4 +2,4 @@
 
 [assembly:AssemblyTitle("Junior.Common.Net40")]
 [assembly:AssemblyVersion("5.0.0.0")]
-[assembly:AssemblyFileVersion("5.0.1.0")]
+[assembly:AssemblyFileVersion("5.0.2.0")]
--- Source/Common.Net45/Properties/AssemblyInfo.cs
+++ Source/Common.Net45/Properties/AssemblyInfo.cs
@@ -2,4 +2,4 @@
 
 [assembly:AssemblyTitle("Junior.Common.Net45")]
 [assembly:AssemblyVersion("5.0.0.0")]
-[assembly:AssemblyFileVersion("5.0.1.0")]
+[assembly:AssemblyFileVersion("5.0.2.0")]
