070ea360379ab541e988d0631eafae4513236d4f
Nathan Alden, Sr <me@nathanalden.com>
2014-07-02 09:25:33 -0400

    Added EnsureNotNull extension method for GUIDs

---
+++ Source/Common.Net35/GuidExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Junior.Common.Net35
+{
+    /// <summary>
+    /// Extension methods for the <see cref="Guid"/> type.
+    /// </summary>
+    public static class GuidExtensions
+    {
+        /// <summary>
+        /// Throws an <see cref="ArgumentException"/> if the specified value is <see cref="Guid.Empty"/>; otherwise, returns the value.
+        /// </summary>
+        /// <param name="value">A value.</param>
+        /// <param name="paramName">The value's parameter name.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is <see cref="Guid.Empty"/>.</exception>
+        /// <returns>the specified value</returns>
+        public static Guid EnsureNotNull(this Guid value, string paramName)
+        {
+            if (value == Guid.Empty)
+            {
+                throw new ArgumentException("Value cannot be empty.", paramName);
+            }
+
+            return value;
+        }
+    }
+}
---
+++ Source/UnitTests/Common.Net35/GuidExtensionsTester.cs
@@ -0,0 +1,33 @@
+using System;
+
+using Junior.Common.Net35;
+
+using NUnit.Framework;
+
+namespace Junior.Common.UnitTests.Common.Net35
+{
+    public static class GuidExtensionsTester
+    {
+        [TestFixture]
+        public class When_calling_EnsureNotNull_on_non_null_parameter
+        {
+            [Test]
+            public void Must_return_value()
+            {
+                Guid value = Guid.Parse("306fb37d-ff3c-47ef-8eca-f47f4dfcb3f5");
+
+                Assert.That(value.EnsureNotNull("value"), Is.EqualTo(value));
+            }
+        }
+
+        [TestFixture]
+        public class When_calling_EnsureNotNull_on_null_parameter
+        {
+            [Test]
+            public void Must_throw_exception()
+            {
+                Assert.Throws<ArgumentException>(() => Guid.Empty.EnsureNotNull("value"));
+            }
+        }
+    }
+}
